f575d437ca1803dc0a72f9559c26a62778b388c5
Alvian Wahyudhi <nogotskil@gmail.com>
2018-10-11 10:49:49 +0800

    FITUR TAMBAHAN SP
    jika sebelumnya diisi, yang selanjutnya tidak tampil. namun jika udah diisi, sekrang dan sebelumnya tampil

--- SP/D/B02.php
+++ SP/D/B02.php
@@ -1,3 +1,30 @@
+<?php 
+include "../../storage.php";
+mysql_connect("localhost", "root", "") or
+    die("Could not connect: " . mysql_error());
+mysql_select_db("smhk");
+
+function cek(){
+  $tanggal = date('Ymd');
+  $cek = mysql_query("SELECT * FROM spub WHERE tanggal = '$tanggal' and nama_tangki='D'") or die (mysql_error());
+  $cekin = mysql_fetch_array($cek);
+  if ($cekin['six'] == null) {
+    echo "<li><a href='B02-INPUT-06.php'>INPUT 06.00</a></li>";
+  }elseif ($cekin['twelve'] == null and $cekin['six'] <> null) {
+   echo "<li><a href='B02-INPUT-06.php'>INPUT 06.00</a></li>
+   <li><a href='B02-INPUT-12.php'>INPUT 12.00</a></li>";
+  }elseif ($cekin['eightteen'] == null and $cekin['six'] <> null) {
+    echo "<li><a href='B02-INPUT-06.php'>INPUT 06.00</a></li>
+   <li><a href='B02-INPUT-12.php'>INPUT 12.00</a></li>
+   <li><a href='B02-INPUT-18.php'>INPUT 18.00</a></li>";
+  }else{
+    echo "<li><a href='B02-INPUT-06.php'>INPUT 06.00</a></li>
+   <li><a href='B02-INPUT-12.php'>INPUT 12.00</a></li>
+   <li><a href='B02-INPUT-18.php'>INPUT 18.00</a></li>
+   <li><a href='B02-INPUT-24.php'>INPUT 24.00</a></li>";
+  }
+}
+ ?>
 <html>
 <head>
     <meta charset="utf-8">
@@ -15,10 +42,7 @@
       <a class="navbar-brand" href="../../index.php">Pertamina EP Asset 5</a>
     </div>
     <ul class="nav navbar-nav">
-      <li><a href="B02-INPUT-06.php">INPUT 06.00</a></li>
-      <li><a href="B02-INPUT-12.php">INPUT 12.00</a></li>
-      <li><a href="B02-INPUT-18.php">INPUT 18.00</a></li>
-      <li><a href="B02-INPUT-24.php">INPUT 24.00</a></li>
+      <?php cek(); ?>
       <li><a href="../SP.php"><< PILIH SP</a></li>
     </ul>
   </div>
